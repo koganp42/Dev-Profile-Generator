19c1687fa57902e8943abda3b9ae9628654e0ab4
Kogan <koganp42@gmail.com>
2019-12-31 15:25:04 -0600

    Added global variables for pdf generation and inquirer prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,40 @@
 const inquirer = require("inquirer");
-const generateHTML = require("./generateHTML.js"); 
-const axios= require("axios"); 
-const fs= require("fs"),
-  convertFactory= require("electron-html-to"); 
+const generateHTML = require("./generateHTML.js");
+const axios = require("axios");
+const fs = require("fs"),
+    convertFactory = require("electron-html-to");
 const conversion = convertFactory({
-  converterPath: convertFactory.converters.PDF
-}); 
+    converterPath: convertFactory.converters.PDF
+});
 
-const questions = [
+let bioImage = null;
+let userLocation = null;
+let userGithubLink = null;
+let publicRepoNum = null;
+let followersNum = null;
+let githubStarsNum = null;
+let followingCount = null;
 
-];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Github username?"
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "Pick a color for your profile!",
+            choices: ["green", "blue", "pink", "red"]
+        }
+    ])
+    .then(answers => {
+        // Use user feedback for... whatever!!
+    });
 
 function writeToFile(fileName, data) {
- 
+
 }
 
 function init() {
