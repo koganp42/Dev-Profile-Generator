90814adff40c5f8eafb596b965183fbe6a7ffcf3
Kogan <koganp42@gmail.com>
2019-12-31 13:14:04 -0600

    Copied over starting js files

---
+++ Develop/index.js
@@ -0,0 +1,13 @@
+const questions = [
+    {color: "What is your favorite color?",
+    username: "What is your Github username?"
+    }
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+}
+init();
