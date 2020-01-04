8d1fd3a2c9809b610bb7e65fe1a675cffcd4b005
Kogan <koganp42@gmail.com>
2020-01-04 16:47:32 -0600

    Have the github api calls working and saving to variables.

--- Develop/index.js
+++ Develop/index.js
@@ -7,13 +7,17 @@ const conversion = convertFactory({
     converterPath: convertFactory.converters.PDF
 });
 
-let bioImage = null;
+let profImage = null;
+let userName = null;
 let userLocation = null;
 let userGithubLink = null;
+let userBlog = null;
+let userBio = null;
 let publicRepoNum = null;
 let followersNum = null;
 let githubStarsNum = null;
 let followingCount = null;
+
 
 inquirer
     .prompt([
@@ -29,8 +33,31 @@ inquirer
             choices: ["green", "blue", "pink", "red"]
         }
     ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
+    .then(function(response){
+        let { githubLogin } = response;
+        let colors = response;
+        let queryUrl = `https://api.github.com/users/${githubLogin}`;
+        axios.get(queryUrl)
+            .then(function(response_git){
+                profImage = response_git.avatar_url;
+                userName = response_git.name;
+                userGithubLink = response_git.html_url;
+                userBlog = response_git.blog;
+                userBio = response_git.bio;
+                publicRepoNum = response_git.public_repos;
+                followersNum = response_git.followers;
+                followingCount = response_git.following;
+                userLocation = response_git.location;
+                //console.log();
+            });
+        axios.get(queryUrl + "/repos")
+            .then(function(response){
+                response.data.forEach(function(stars){
+                    githubStarsNum += stars.stargazers_count;
+                    
+                });
+                console.log(githubStarsNum);
+            });
     });
 
 function writeToFile(fileName, data) {
