2e439da1f693fb9b6de4e2bd1371af09090b131c
Kogan <koganp42@gmail.com>
2020-01-05 17:13:49 -0600

    Changed everything to use a global object and async function structure

--- Develop/index.js
+++ Develop/index.js
@@ -7,73 +7,80 @@ const conversion = convertFactory({
     converterPath: convertFactory.converters.PDF
 });
 
-let profImage = null;
-let userName = null;
-let userLocation = null;
-let userGithubLink = null;
-let userCompany = null;
-let userBlog = null;
-let userBio = null;
-let publicRepoNum = null;
-let followersNum = null;
-let githubStarsNum = null;
-let followingNum = null;
 const googleApiKey = "AIzaSyAPg5Dwcw9-O5hcA9WLjNALXCtfjTJLa8s";
 
+let htmlObj = {
+    profImage: null, 
+    userName: null, 
+    googleMapUrl: null, 
+    userLocation: null, 
+    userGithubLink: null, 
+    userBlog: null, 
+    userBio: null, 
+    publicRepoNum: null, 
+    followersNum: null, 
+    githubStarsNum: 0, 
+    followingNum: null,
+    colors: null
+};
 
-function init() {
+async function inq() { 
+    return inquirer.prompt([
+       {
+           type: "input",
+           name: "github",
+           message: "What is your Github username?"
+       },
+       {
+           type: "list",
+           name: "color",
+           message: "Pick a color for your profile!",
+           choices: ["green", "blue", "pink", "red"]
+       }
+ ])}
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "github",
-            message: "What is your Github username?"
-        },
-        {
-            type: "list",
-            name: "color",
-            message: "Pick a color for your profile!",
-            choices: ["green", "blue", "pink", "red"]
-        }
-    ])
-    .then(function(response){
-        let githubLogin = response.github;
-        let colors = response.color;
-        let queryUrl = `https://api.github.com/users/${githubLogin}`;
-        axios.get(queryUrl)
-            .then(function(response_git){
-                profImage = response_git.data.avatar_url;
-                userName = response_git.data.name;
-                console.log(userName);
-                userGithubLink = response_git.data.html_url;
-                userBlog = response_git.data.blog;
-                userBio = response_git.data.bio;
-                userCompany = response_git.data.company;
-                publicRepoNum = response_git.data.public_repos;
-                followersNum = response_git.data.followers;
-                followingNum = response_git.data.following;
-                userLocation = response_git.data.location;
-                console.log(userLocation);
-                let googleGeoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=${googleApiKey}`;
-                console.log(googleGeoUrl);
-                return axios.get(googleGeoUrl);
-            }).then(function(geo_response){
-                console.log(geo_response.data.geometry);
-                let userLat = geo_response.data.geometry.location.lat;
-                let userLong = geo_response.data.geometry.location.lng;
-            });
-            
-        axios.get(queryUrl + "/repos")
-            .then(function(response_git_stars){
-                response_git_stars.data.forEach(function(stars){
-                    githubStarsNum += stars.stargazers_count;
-                });
-                console.log(githubStarsNum);
-            });
-        //let generateHTML = html;
+async function githubProfileRequest(githubLogin){
+    let queryUrl = `https://api.github.com/users/${githubLogin}`;
+    return axios.get(queryUrl);
+}
 
+async function githubRepoRequest(githubLogin){
+  	let queryUrl = `https://api.github.com/users/${githubLogin}/repos`;
+  	return axios.get(queryUrl);
+}
+
+async function googleGeoCodeRequest(){
+  	let queryUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${htmlObj.userLocation}&key=${googleApiKey}`;
+		return axios.get(queryUrl);
+}
+
+async function init() {
+  	let userPromptResponse = await inq();
+  	htmlObj.colors = userPromptResponse.color;	
+  
+  	let githubProfileResponse = await githubProfileRequest(userPromptResponse.github);
+    let githubRes = githubProfileResponse.data;
+    htmlObj.profImage = githubRes.avatar_url;
+    htmlObj.userName = githubRes.name;
+    htmlObj.userGithubLink = githubRes.html_url;
+    htmlObj.userBlog = githubRes.blog;
+    htmlObj.userBio = githubRes.bio;
+    htmlObj.userCompany = githubRes.company;
+    htmlObj.publicRepoNum = githubRes.public_repos;
+    htmlObj.followersNum = githubRes.followers;
+    htmlObj.followingNum = githubRes.following;
+    htmlObj.userLocation = githubRes.location;
+    console.log(htmlObj.userLocation);
+
+  	let githubRepoResponse = await githubRepoRequest(userPromptResponse.github);
+  	githubRepoResponse.data.forEach(function(repo){
+      htmlObj.githubStarsNum += repo.stargazers_count;
     });
-
+  
+  	let googleGeoCodeResponse = await googleGeoCodeRequest();
+    let userLat = googleGeoCodeResponse.data.results[0].geometry.location.lat;
+    let userLng = googleGeoCodeResponse.data.results[0].geometry.location.lng;
+    htmlObj.googleMapUrl = `https://www.google.com/maps/@${userLat},${userLng},13z`
+    console.log(htmlObj);
 }
 init();
