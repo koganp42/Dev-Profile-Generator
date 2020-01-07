c4a1969369d54d1d4267f1f69ad946d78a5216f6
Kogan <koganp42@gmail.com>
2020-01-07 17:20:39 -0600

    Removed api key, finished readme

--- Develop/index.js
+++ Develop/index.js
@@ -7,56 +7,57 @@ const conversion = convertFactory({
     converterPath: convertFactory.converters.PDF
 });
 
-const googleApiKey = "AIzaSyAPg5Dwcw9-O5hcA9WLjNALXCtfjTJLa8s";
+const googleApiKey = "";
 
-let profImage = null; 
+let profImage = null;
 let userName = null;
-let googleMapUrl = null; 
-let userLocation = null; 
-let userGithubLink = null; 
-let userBlog = null; 
-let userBio = null; 
-let publicRepoNum = null; 
-let followersNum = null; 
-let githubStarsNum = 0; 
+let googleMapUrl = null;
+let userLocation = null;
+let userGithubLink = null;
+let userBlog = null;
+let userBio = null;
+let publicRepoNum = null;
+let followersNum = null;
+let githubStarsNum = 0;
 let followingNum = null;
 let data = null;
 
-async function inq() { 
+async function inq() {
     return inquirer.prompt([
-       {
-           type: "input",
-           name: "github",
-           message: "What is your Github username?"
-       },
-       {
-           type: "list",
-           name: "color",
-           message: "Pick a color for your profile!",
-           choices: ["green", "blue", "pink", "red"]
-       }
- ])}
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Github username?"
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "Pick a color for your profile!",
+            choices: ["green", "blue", "pink", "red"]
+        }
+    ])
+}
 
-async function githubProfileRequest(githubLogin){
+async function githubProfileRequest(githubLogin) {
     let queryUrl = `https://api.github.com/users/${githubLogin}`;
     return axios.get(queryUrl);
 }
 
-async function githubRepoRequest(githubLogin){
-  	let queryUrl = `https://api.github.com/users/${githubLogin}/repos`;
-  	return axios.get(queryUrl);
+async function githubRepoRequest(githubLogin) {
+    let queryUrl = `https://api.github.com/users/${githubLogin}/repos`;
+    return axios.get(queryUrl);
 }
 
-async function googleGeoCodeRequest(){
-  	let queryUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=${googleApiKey}`;
-		return axios.get(queryUrl);
+async function googleGeoCodeRequest() {
+    let queryUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=${googleApiKey}`;
+    return axios.get(queryUrl);
 }
 
 async function init() {
-  	let userPromptResponse = await inq();
-  	data = userPromptResponse;	
-  
-  	let githubProfileResponse = await githubProfileRequest(userPromptResponse.github);
+    let userPromptResponse = await inq();
+    data = userPromptResponse;
+
+    let githubProfileResponse = await githubProfileRequest(userPromptResponse.github);
     let githubRes = githubProfileResponse.data;
     profImage = githubRes.avatar_url;
     userName = githubRes.name;
@@ -70,26 +71,26 @@ async function init() {
     userLocation = githubRes.location;
     console.log(userLocation);
 
-  	let githubRepoResponse = await githubRepoRequest(userPromptResponse.github);
-  	githubRepoResponse.data.forEach(function(repo){
-      githubStarsNum += repo.stargazers_count;
+    let githubRepoResponse = await githubRepoRequest(userPromptResponse.github);
+    githubRepoResponse.data.forEach(function (repo) {
+        githubStarsNum += repo.stargazers_count;
     });
-  
-  	let googleGeoCodeResponse = await googleGeoCodeRequest();
+
+    let googleGeoCodeResponse = await googleGeoCodeRequest();
     let userLat = googleGeoCodeResponse.data.results[0].geometry.location.lat;
     let userLng = googleGeoCodeResponse.data.results[0].geometry.location.lng;
     googleMapUrl = `https://www.google.com/maps/@${userLat},${userLng},11z`
 
     const htmlPageData = generateHTML.generateHTML(data, profImage, userName, googleMapUrl, userLocation, userGithubLink, userBlog, userBio, publicRepoNum, followersNum, githubStarsNum, followingNum);
-    
-    conversion({html: htmlPageData, delay: 1000}, function(err, result){
-        if (err){
-          return console.log(err); 
-        } 
-        console.log(result.numberOfPages); 
+
+    conversion({ html: htmlPageData, delay: 1000 }, function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(result.numberOfPages);
         console.log("success");
         result.stream.pipe(fs.createWriteStream(`${userName}.pdf`));
-        conversion.kill(); 
+        conversion.kill();
     });
 }
 init();
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Dev-Profile-Generator
+A command line application that generates a profile synopsis in pdf based on a Github username.
+
+## How To
+Simply open the main index.js file in terminal, and enter node index.js; then enter any valid github username and select a color.
+
+### What You Get
+Assuming the github user's profile has corresponding information filled out, the pdf will display:
+-Profile image
+-User name
+-Links to the following:
+    *User location via Google Maps
+    *User GitHub profile
+    *User blog
+-User bio
+-Number of public repositories
+-Number of followers
+-Number of GitHub stars
+-Number of users following
+
+## Made With
+-Javascript
+-node.js
+-Github API
+-Google GeoLocation API
